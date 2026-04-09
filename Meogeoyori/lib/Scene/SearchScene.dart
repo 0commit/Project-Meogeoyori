e9591cf1e022faa974f205e1e19256c78c685d9b
0commit <ggrty10@gmail.com>
2026-04-09 12:02:28 +0900

    feat: Setting Tab

---
+++ Meogeoyori/lib/Scene/SearchScene.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SearchScene extends StatelessWidget{
+  const SearchScene({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Search"),
+      )
+    );
+  }
+}
---
+++ Meogeoyori/lib/Scene/TimerScene.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TimerScene extends StatelessWidget {
+  const TimerScene({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+        body: Center(
+          child: Text("Timer")
+        ),
+      );
+  }
+}
